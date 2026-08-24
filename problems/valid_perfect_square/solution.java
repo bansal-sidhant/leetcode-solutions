e6b7c2eac5931541533a4c81bc797a73fbea92aa
bansal-sidhant <sidb20@gmail.com>
2026-08-24 00:35:14 +0000

    Sync LeetCode submission - Valid Perfect Square (java)

---
+++ problems/valid_perfect_square/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+
+        while(left<= right) {
+            int mid = left + (right - left)/2;
+            
+            if((long) mid * mid  == num) {
+                return true;
+            } else if((long) mid * mid < num) {
+                left = mid + 1;
+            } else {
+                right = mid -1;
+            }
+        }
+        return false;
+    }
+}
